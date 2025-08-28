adc76586145b618e4bc5012e1d88a4cc194d1603
hyeridfd <160723285+hyeridfd@users.noreply.github.com>
2025-08-28 14:54:45 +0900

    Update soymelier_challenge.py

--- soymelier_challenge.py
+++ soymelier_challenge.py
@@ -641,7 +641,7 @@ def main():
           </div>
       </div>
     </div>
-    
+     """)
     <style>
     /* 기존 것 유지 */
     html, body { margin-bottom: 0 !important; padding-bottom: 0 !important; }
@@ -696,7 +696,6 @@ def main():
       padding-bottom: 0 !important;
     }
     </style>
-    """)
 
 
 def home_page():
